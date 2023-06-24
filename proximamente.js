2ba0fc940361516c1d82d2fb8345bc382365578d
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-06-24 17:14:50 -0600

    Add basic structure

---
+++ proximamente.js
@@ -0,0 +1,28 @@
+const proximamenteElement = document.getElementById('proximamente-text');
+let proximamenteTexto = 'pagina en construcción 🚧';
+let proximamenteIndice = 0;
+let cursorVisible = false;
+
+function escribirProximamente() {
+    if (proximamenteIndice < proximamenteTexto.length) {
+        proximamenteElement.textContent += proximamenteTexto.charAt(proximamenteIndice);
+        proximamenteIndice++;
+    } else {
+        if (cursorVisible) {
+            proximamenteElement.textContent = proximamenteElement.textContent.slice(0, -1);
+            cursorVisible = false;
+        } else {
+            proximamenteElement.textContent += '_';
+            cursorVisible = true;
+        }
+    }
+    setTimeout(escribirProximamente, 200);
+}
+
+if(proximamenteElement){
+    escribirProximamente();
+}
+
+document.getElementById("boton-volver").addEventListener("click", function() {
+    window.location.href = "main.html";
+});
