13d95cda496146eef10e19a38a999d9f6b603fa9
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2024-01-29 16:10:18 -0600

    Update main page title

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ secciones.forEach((seccion) => {
 // función para el efecto de "escritura" del párrafo
 let textoParrafo = "Somos la sociedad secreta de productores musicales y beatmakers dedicada a ofrecer productos musicales de alta gama para cualquiera que los requiera...";
 let k = 0; // indice para recorrer el texto del parrafo
-let velocidadParrafo = 70; // ajusta la velocidad de escritura del parrafo
+let velocidadParrafo = 100; // ajusta la velocidad de escritura del parrafo
 
 function escribirParrafo() {
     if (k < textoParrafo.length) {
@@ -33,7 +33,7 @@ function escribirParrafo() {
 escribirParrafo(); // iniciamos la animación
 
 // Tu función existente
-var texto2 = "S.S.R.";
+var texto2 = "Sociedad Secreta del Ritmo";
 var j = 0;
 var velocidad = 500; // velocidad de la animación de escritura
 
